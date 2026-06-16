09f666fdbb739d8ce365582a48b2246b3c7b921c
Alexander Degtyarev <adegtyarev.ap@gmail.com>
2026-06-17 00:13:05 +0400

    chore: bump to v0.17.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "org.openwebdav.messenger"
         minSdk = 26
         targetSdk = 35
-        versionCode = 39
-        versionName = "0.17.6"
+        versionCode = 40
+        versionName = "0.17.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
