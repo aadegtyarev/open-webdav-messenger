3f3b3f6128819274b370a749ecaa2667b3e50f30
Alexander Degtyarev <adegtyarev.ap@gmail.com>
2026-06-16 21:48:37 +0400

    chore: bump to v0.16.6 (fixed update button)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "org.openwebdav.messenger"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "0.16.5"
+        versionCode = 29
+        versionName = "0.16.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
